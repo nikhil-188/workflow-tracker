f081cb9e8df0348a4f77252ce37795815aa32459
Nikhil Parasu <nikhil.parasu517@gmail.com>
2026-02-05 16:48:35 +0530

    Initial project setup for Workflow Tracker

---
+++ src/main/java/com/workflowtracker/WorkflowTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.workflowtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowTrackerApplication.class, args);
+	}
+
+}
